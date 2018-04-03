dd90543fd1c433f8742e709529af19920babbca1
flamel13 <thesorcerer13@gmail.com>
2018-04-03 10:45:34 +0200

    Models updated
    models updated.

--- acmeconf/reservations/models.py
+++ acmeconf/reservations/models.py
@@ -6,4 +6,18 @@ class Event(models.Model):
     location = models.CharField(max_length=200)
     seats = models.IntegerField(default=0)
     date = models.DateTimeField('date published')
-    
+
+class EventFee(models.Model):
+    event = models.ForeignKey(Event, related_name='fee_options', on_delete=models.DO_NOTHING)
+    available = models.BooleanField(default=True)
+    name = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=255,decimal_places=2)
+
+class EventChoice(models.Model):
+    event = models.ForeignKey(Event, related_name='choices', on_delete=models.DO_NOTHING)
+    order = models.PositiveIntegerField(default=0)
+    name = models.CharField(max_length=32)
+    label = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    required = models.BooleanField(default=False)
+    allow_multiple = models.BooleanField(default=False)
