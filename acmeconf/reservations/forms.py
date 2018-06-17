64008f4fd1c2505077cb94cbaf8b84c1bf43fc4d
Michele Contu <thesorcerer13@gmail.com>
2018-06-17 23:17:42 +0200

    Password input field of the bank form is masked now
    Password input field of the bank form is masked now

--- acmeconf/reservations/forms.py
+++ acmeconf/reservations/forms.py
@@ -14,8 +14,8 @@ class UserForm(forms.ModelForm):
 
 class EventReservationForm(forms.ModelForm):
     name = forms.CharField(label='Name', max_length=100)
-    password = forms.CharField(label='Password', max_length=100)
-
+    password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
+    
     class Meta:
         model = EventReservation
         fields = ['event' , 'is_staff']
